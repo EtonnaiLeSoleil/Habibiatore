12ffc7312edc6578dec9a99001084d804b2bdfeb
Antoine.Gouet <antoine.gouet@3wacademy.fr>
2025-02-20 13:35:25 +0000

    aki terminé manque plus que il afiche les réponse + que je rajoute une page pour lancer le jeux

--- secret.php
+++ secret.php
@@ -2,42 +2,53 @@
 include "config/database.php";
 include "repository/questionRepository.php";
 
+session_start();
+
 
 $questions = getAllQuestions();
 print_r($questions);
 
-$question_id = 29; // Replace with the ID of the question you want to retrieve
-$question = getQuestionId($question_id);
+// Si un question_id est défini dans la session, l'utiliser, sinon utiliser l'ID par défaut
+if (isset($_SESSION["question_id"])) {
+    $question_id = (int)$_SESSION["question_id"];
+} else {
+    $question_id = 29; // ID par défaut
+}
 
+$question = getQuestionId($question_id);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $yn = $_POST["YN"];
     switch ($yn) {
         case "Oui":
-      
-            echo "C'est un chien.";
-            
-            
-            if (isset($question["next_question_id_y"])) {
-              $question_id = $question["next_question_id_y"];
-              $question = getQuestionId($question_id);
+            if(isset($question["id_planet_y"])){
+                $_SESSION["planet_id"] = $question["id_planet_y"];
+                
+                echo("caca");
             }
-            
-            
-        break;
+            elseif (isset($question["next_question_id_y"])) {
+                $_SESSION["question_id"] = $question["next_question_id_y"];
+                $question_id = $question["next_question_id_y"];
+                $question = getQuestionId($question_id);
+            }
+            break;
         case "Non":
-            echo "C'est un oiseau.";
-            
-            if (isset($question["next_question_id_n"])) {
-              $question_id = $question["next_question_id_n"];
-              $question = getQuestionId($question_id);
+            if(isset($question["id_planet_n"])){
+                $_SESSION["planet_id"] = $question["id_planet_n"];
+                
+                echo("caca");
+            }
+            elseif (isset($question["next_question_id_n"])) {
+                $_SESSION["question_id"] = $question["next_question_id_n"];
+                $question_id = $question["next_question_id_n"];
+                $question = getQuestionId($question_id);
             }
             
             break;
     }
 }
 
-
+var_dump($_SESSION);
 
 $template = "secret";
 include "layout.phtml";
